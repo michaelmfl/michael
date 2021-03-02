da4eadbe8a3ac606d4c6bd222939ad1d527a2b17
michael mfl <58003940+michaelmfl@users.noreply.github.com>
2021-03-02 03:03:48 +0200

    Add files via upload

---
+++ putchar/putchar1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (void){
+int
+ ch ='G';
+putchar(ch);
+return (0);
+}
---
+++ putchar/putchar2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void){
+    int ch;
+    ch ='1';
+    for (ch= '1'; ch < '9'; ch++)
+    {
+        putchar(ch);
+    }
+    return (0);
+}
